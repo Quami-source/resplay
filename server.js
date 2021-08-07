dad720424d85354a078b19e3ae8b2258b2cbf4a3
Quami-source <emmmanuelquami@outlook.com>
2021-08-07 03:56:49 +0000

    Made some changes to the signup url

--- server.js
+++ server.js
@@ -11,26 +11,39 @@ app.get("/", (req, res) => {
   res.send("Hello there");
 });
 
+app.post("/post", (req, res) => {
+  res.json({
+    message: "Post created",
+  });
+});
+
 //authenticate user
 app.post("/signup", async (req, res) => {
-  const userID = req.body.uid;
-  const pwd = req.body.password;
-  //hash password
-  const hash = bcrypt.hash(pwd, 12, (e) => console.log(e));
-  //check if user exists
-  const checkUser = Users.findOne({ uid: userID });
-  if (checkUser) {
-    res.send("User exists");
-  } else {
-    const newUser = new Users({
-      uid: userID,
-      password: hash,
-    });
-    //generate token
-    const token = jwt.sign({ user: userID }, "shhhhhh");
-    const result = await newUser.save();
-    res.json({ token, result });
-  }
+  //console.log(req.query);
+  res.json({
+    message: "Request received",
+    data: req.query,
+  });
+  // const userID = req.body.uid;
+  // const pwd = req.body.password;
+  // //hash password
+  // const hash = bcrypt.hash(pwd, 12, (e) => console.log(e));
+  // //check if user exists
+  // const checkUser = await Users.findOne({ uid: userID });
+  // if (checkUser) {
+  //   res.send("User exists");
+  // } else {
+  //   const newUser = new Users({
+  //     uid: userID,
+  //     password: hash,
+  //   });
+  //   //generate token
+  //   const token = jwt.sign({ user: userID }, "shhhhhh", (err, res) => {
+  //     if (err) res.json({ message: "error with jwt" });
+  //   });
+  //   const result = await newUser.save();
+  //   res.json({ token: token, result: result });
+  // }
 });
 
 //sign in user
